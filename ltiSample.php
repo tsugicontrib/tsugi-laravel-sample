433e192de4616d493f04f370d888caecbad46a6e
Charles Severance <csev@umich.edu>
2016-11-26 12:29:50 -0500

    Create ltiSample.php

---
+++ ltiSample.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+require_once app_path() ."/../config.php";
+
+use Illuminate\Http\Request;
+use Tsugi\Laravel\LTIX;
+
+class ltiSample extends Controller
+{
+
+    public function hello(Request $request) {
+
+        $launch = LTIX::laravelSetup($request, LTIX::ALL);
+        if ( $launch->redirect_url ) return redirect($launch->redirect_url);
+        if ( $launch->send_403 ) return response($launch->error_message, 403);
+
+        ob_start();
+        echo("<pre>\n");
+        echo("\nLaunch:\n");
+        var_dump($launch);
+/*
+        echo("\nSession:\n");
+        var_dump($request->session());
+        echo("\nPost:\n");
+        var_dump($_POST);
+
+        global $CFG;
+        echo("\nCFG:\n");
+        var_dump($CFG);
+*/
+        echo("</pre>\n");
+        return ob_get_clean();
+    }
+}
